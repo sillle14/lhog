8e8543dbce171854c8d35f87e8295af42aa9ef4f
Lewis Silletto <lsilletto@caredash.com>
2020-06-30 09:58:33 -0400

    done with regions

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -7,15 +7,14 @@ export class WattMatrixTable extends React.Component {
     render () {
         // TODO: Style the scrollbar so it always shows up against the white map bette
         return (
-            // <div id="board">
-            //     <div id="main">
-            //         <div id="players"></div>
-            //         <Map cityStatus={this.props.G.cityStatus}></Map>
-            //     </div>
-            //     <div id="log"></div>
-            //     <div id="action"></div>
-            // </div>
-            <Map cityStatus={this.props.G.cityStatus}></Map>
+            <div id="board">
+                <div id="main">
+                    <div id="players"></div>
+                    <Map cityStatus={this.props.G.cityStatus}></Map>
+                </div>
+                <div id="log"></div>
+                <div id="action"></div>
+            </div>
         )
     }
 }
