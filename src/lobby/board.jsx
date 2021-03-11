dadaaa98694dddb2786352baeeb8aa015ebb30b0
Lewis Silletto <lsilletto@caredash.com>
2021-03-10 21:39:06 -0500

    if it aint broke

--- src/lobby/board.jsx
+++ src/lobby/board.jsx
@@ -18,7 +18,7 @@ export default function Board({runningMatch}) {
         if (!runningMatch) {
             navigate('/')
         }
-    }, [runningMatch, navigate])
+    })
 
     if (runningMatch) {
         return (
